9b1fd728f8543c55780a2013ba892bc310cd3dde
LIK-HO <ivanov.eugene.y@gmail.com>
2026-09-18 02:27:09 +0300

    fix(v1.4): satisfy ruff type annotation rules

--- src/shema_platform/foundation/ids.py
+++ src/shema_platform/foundation/ids.py
@@ -11,11 +11,11 @@ class Id:
     value: UUID
 
     @classmethod
-    def new(cls) -> "Id":
+    def new(cls) -> Id:
         return cls(uuid4())
 
     @classmethod
-    def parse(cls, value: str | UUID) -> "Id":
+    def parse(cls, value: str | UUID) -> Id:
         return cls(value if isinstance(value, UUID) else UUID(value))
 
     def __str__(self) -> str:
