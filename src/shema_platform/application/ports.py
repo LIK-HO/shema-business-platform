05907c7e699d8e3059749e7aa633c9061162df12
LIK-HO <ivanov.eugene.y@gmail.com>
2026-09-20 00:36:25 +0300

    fix(v1.4): expose jobs in unit-of-work contract

--- src/shema_platform/application/ports.py
+++ src/shema_platform/application/ports.py
@@ -148,6 +148,7 @@ class UnitOfWork(Protocol):
     audits: AuditRepository
     idempotency: IdempotencyRepository
     outbox: OutboxRepository
+    jobs: JobRepository
     commercial_actions: CommercialActionRepository
     orders: OrderRepository
     economics: EconomicEntryRepository
