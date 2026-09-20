1f176027e130ba9c543a0f405bd09cacbd13c925
LIK-HO <ivanov.eugene.y@gmail.com>
2026-09-20 09:07:09 +0300

    Fix commercial action import ordering

--- src/shema_platform/domain/commercial_action.py
+++ src/shema_platform/domain/commercial_action.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from datetime import datetime
-
 from enum import StrEnum
 
 from shema_platform.foundation.errors import QuarantineRequired
