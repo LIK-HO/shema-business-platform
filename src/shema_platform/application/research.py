71d892348818f8ea8859c4dcdd041772d2f19427
LIK-HO <ivanov.eugene.y@gmail.com>
2026-09-23 21:03:38 +0300

    v1.5: normalize resource guard import

--- src/shema_platform/application/research.py
+++ src/shema_platform/application/research.py
@@ -4,7 +4,10 @@ from dataclasses import dataclass
 from math import isfinite
 from typing import Protocol
 
-from shema_platform.foundation.provider_resource_guard import ProviderCallDecision, ProviderResourceGuard
+from shema_platform.foundation.provider_resource_guard import (
+    ProviderCallDecision,
+    ProviderResourceGuard,
+)
 
 
 @dataclass(frozen=True, slots=True)
